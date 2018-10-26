0718ffbec8fd8ca5161dc6259ba03d734a47f7c1
Haibo Tu <tuhaibo511@126.com>
2018-10-26 05:07:23 -0400

    Commit Message: Add personSet shell scripts for ubuntu.
    JIRA ID:SSRS-debug
    Bug Fix [Y/N]:N
    
    	new file:   git/.gitmsg
    	new file:   ubuntuPersonalSet/ubuntuPersonalSet.sh

---
+++ ubuntuPersonalSet/ubuntuPersonalSet.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Install the cscope software
+function installSoftware()
+{
+    sudo apt-get install cscope
+    sudo apt-get install ctags
+    sudo apt-get install git
+    sudo apt-get install vim
+    sudo apt-get install ssh
+}
+
+#copy the default config file
+function cpConfigFile()
+{
+    cp ../vim/.vimrc ~/
+    cp ../git/.gitmsg ~/
+}
+
+#config the git 
+function gitConfig()
+{
+    git config --global color.ui true
+    git config --global user.email "tuhaibo511@126.com"
+    git config --global user.name "Haibo Tu"
+    git config --global core.editor vim
+    git config --global commit.template ~/.gitmsg
+    git config --global push.default simple
+    git config --global credential.helper store
+}
+
+installSoftware
+gitConfig
+cpConfigFile
